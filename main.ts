377b20859f2bf30f6656f38138ad49ef18cd1021
hexapodo2 <62761445+hexapodo2@users.noreply.github.com>
2021-08-12 15:28:51 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    music.playMelody("E B C5 A B G A F ", 120)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Tortoise)
+})
